ede0ccd32f81edbeacd40f54412981b86a8a0467
Alan Rafael R. Batista <alan@campuscode.com.br>
2018-11-14 17:41:25 -0200

    First step to production

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_03_200953) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cuisines", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
